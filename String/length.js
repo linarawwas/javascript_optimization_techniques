ed93687fae3ba94614f70e05a054c0540da63507
linarawwas <linamkraw2@gmail.com>
2023-11-16 16:21:32 +0200

    added length

---
+++ String/length.js
@@ -0,0 +1,8 @@
+// ### Length-related Methods:
+// 1. **String length**
+//    - Returns the length of the string.
+//    - **Runtime:** O(1)
+
+// const str = 'Hello, World!';
+// const length = str.length;
+// console.log(length); // Output: 13
